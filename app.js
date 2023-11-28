1be4b1a40860cc9fc7ab9f3c895b93850552f985
Ayodeji Isiaq Oluwasegun <ayodejisq@gmail.com>
2023-11-28 23:20:12 +0100

    Date Added

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const dob = document.querySelector("#dob");
+const dob = document.querySelector(".date");
 const ageCon = document.querySelector(".ageContainer");
-const date = new Date();
 
 const update = () => {
   const date = new Date();
+  const period = date.getHours() > 12 ? "PM" : "AM";
   let hours = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
   let minutes = date.getMinutes();
   let sec = date.getSeconds();
@@ -24,30 +24,60 @@ const update = () => {
     sec = `${sec}`;
   }
 
+  document.querySelector("#date").textContent = `${date.getDate()}/${
+    date.getMonth() + 1
+  }/${date.getFullYear()}`;
   document.querySelector(
-    "#date"
-  ).textContent = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
-  document.querySelector("#time").textContent = `${hours}:${minutes}:${sec}`;
+    "#time"
+  ).textContent = `${hours}:${minutes}:${sec} ${period}`;
 };
 setInterval(update, 1000);
 
-document.querySelector("button").addEventListener("click", (e) => {
+document.querySelector("button").addEventListener("click", () => {
+  const date = new Date();
+
+  let bdMsg = document.querySelector(".bdMsg");
+  let currMonth = date.getMonth() + 1;
   if (dob.value) {
     document.querySelector(".ageInfo").classList.add("hidden");
     if (
-      dob.value.length < 4 ||
-      dob.value.length > 4 ||
-      dob.value > date.getFullYear()
+      dob.value.slice(0, 4).length < 4 ||
+      dob.value.slice(0, 4).length > 4 ||
+      (parseInt(dob.value.slice(0, 4)) == date.getFullYear() &&
+        parseInt(dob.value.slice(8, 10)) > date.getDate() &&
+        parseInt(dob.value.slice(5, 7)) == currMonth)
     ) {
       document.querySelector("#age").textContent = "Invalid Date Of Birth";
       ageCon.classList.remove("hidden");
+      bdMsg.classList.add("hidden");
+    } else if (
+      parseInt(dob.value.slice(0, 4)) == date.getFullYear() &&
+      parseInt(dob.value.slice(8, 10)) <= date.getDate() &&
+      parseInt(dob.value.slice(5, 7)) == currMonth
+    ) {
+      let day = date.getDate() - parseInt(dob.value.slice(8, 10));
+      bdMsg.textContent = `Happy ${day} ${day > 1 ? "days" : "day"} old`;
     } else {
-      let age = date.getFullYear() - parseInt(dob.value);
-      if (age <= 1) {
-        age = `${age}year`;
+      let age = date.getFullYear() - parseInt(dob.value.slice(0, 4));
+      let dobMonth;
+
+      bdMsg.classList.remove("hidden");
+
+      if (parseInt(dob.value.slice(5, 7)) > currMonth) {
+        dobMonth = parseInt(dob.value.slice(5, 7)) - currMonth;
+        bdMsg.textContent = `Your Birthday is ${dobMonth} ${
+          dobMonth > 1 ? "Months" : "Month"
+        } Away. Happy Birthday in Advance!!!`;
+      } else if (parseInt(dob.value.slice(5, 7)) == currMonth) {
+        bdMsg.textContent = `It's Your Month, Happy ${age} Birthday To You!!!`;
       } else {
-        age = `${age}years`;
+        dobMonth = currMonth - parseInt(dob.value.slice(5, 7));
+        bdMsg.textContent = `Congratulations at Your ${age} Birthday. You Already Celeberated Your Birthday ${dobMonth} ${
+          dobMonth > 1 ? "Months" : "Month"
+        } Back. Happy Belated Birthday!!!`;
       }
+      age <= 1 ? (age = `${age}year`) : (age = `${age}years`);
+
       document.querySelector("#age").textContent = age;
 
       document.querySelector("input").value = "";
